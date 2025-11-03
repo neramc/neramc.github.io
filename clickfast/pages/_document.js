0870f0f3ef46fae24dba9037de2e25c6427801b1
Madmovie <kisj0633@icloud.com>
2025-11-03 15:00:42 +0900

    Create _document.js

---
+++ clickfast/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="icon" type="image/png" href="https://i.ifh.cc/wRXc4l.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
